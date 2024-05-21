03b78fa22b84d1f2ec796c10012632480b384e64
Karrtik12 <kartik.chat1212@gmail.com>
2024-05-21 15:42:44 +0530

    material ui v5 + header component/styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,34 @@
+import { SearchOutlined } from "@mui/icons-material";
+import { AppBar, Box } from "@mui/material";
+// import { Autocomplete } from "@react-google-maps/api";
 import React from "react";
+import {
+  Title,
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+  ToolbarStyled,
+} from "./style";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <AppBar position="static">
+      <ToolbarStyled>
+        <Title variant="h5">TravelForecast</Title>
+        <Box display="flex">
+          <Title variant="h6">Forecast Your Adventure</Title>
+          {/* <Autocomplete> */}
+          <Search>
+            <SearchIconWrapper>
+              <SearchOutlined />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search..." />
+          </Search>
+          {/* </Autocomplete> */}
+        </Box>
+      </ToolbarStyled>
+    </AppBar>
+  );
 };
 
 export default Header;
